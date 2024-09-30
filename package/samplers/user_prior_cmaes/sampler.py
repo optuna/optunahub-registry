1b644152415d2b46eb763079d0bb77ab648288a0
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2024-09-30 11:08:09 +0200

    Correct the scale of each param

--- package/samplers/user_prior_cmaes/sampler.py
+++ package/samplers/user_prior_cmaes/sampler.py
@@ -29,16 +29,12 @@ class UserPriorCmaEsSampler(CmaEsSampler):
     This class does not support ``source_trials`` and ``use_separable_cma`` due to their incompatibility.
 
     Args:
-        x0:
-            A dictionary of an initial parameter values for CMA-ES. By default, the mean of ``low``
-            and ``high`` for each distribution is used. Note that ``x0`` is sampled uniformly
-            within the search space domain for each restart if you specify ``restart_strategy``
-            argument.
-
-        sigma0:
-            Initial standard deviation of CMA-ES. By default, ``sigma0`` is set to
-            ``min_range / 6``, where ``min_range`` denotes the minimum range of the distributions
-            in the search space.
+        param_names:
+            The list of the parameter names to be tuned. This list must be a unique list.
+        mu0:
+            The mean vector used for the initialization of CMA-ES.
+        cov0:
+            The covariance matrix used for the initialization of CMA-ES.
     """  # NOQA: E501
 
     def __init__(
@@ -75,12 +71,9 @@ class UserPriorCmaEsSampler(CmaEsSampler):
             source_trials=None,
         )
         self._validate_user_prior(param_names, mu0, cov0)
-        dim = len(param_names)
         self._param_names = param_names[:]
         self._mu0 = mu0.copy()
-        self._sigma0 = math.pow(np.linalg.det(cov0), 1.0 / 2.0 / dim)
-        # Make the determinant of cov0 1 so that it agrees with the CMA-ES convention.
-        self._cov0 = cov0.copy() / self._sigma0**2
+        self._cov0 = cov0.copy()
 
     def _validate_user_prior(
         self, param_names: list[str], mu0: np.ndarray, cov0: np.ndarray
@@ -118,6 +111,44 @@ class UserPriorCmaEsSampler(CmaEsSampler):
 
         return super().sample_relative(study=study, trial=trial, search_space=search_space)
 
+    def _calculate_initial_params(
+        self, trans: _SearchSpaceTransform
+    ) -> tuple[np.ndarray, float, np.ndarray]:
+        search_space = trans._search_space.copy()
+        if any(
+            not isinstance(d, (IntDistribution, FloatDistribution)) for d in search_space.values()
+        ):
+            raise ValueError("search_space cannot include categorical parameters.")
+        if any(
+            d.log
+            for d in search_space.values()
+            if isinstance(d, (FloatDistribution, IntDistribution))
+        ):
+            src_url = "https://hub.optuna.org/samplers/user_prior_cmaes/"
+            raise ValueError(
+                "search_space for user_prior cannot include log scale. "
+                f"Please use the workaround described in {src_url}."
+            )
+
+        dim = len(self._param_names)
+        raw_bounds = trans._raw_bounds
+        domain_sizes = raw_bounds[:, 1] - raw_bounds[:, 0]
+        is_single = domain_sizes == 0.0
+
+        mu0 = self._mu0.copy()
+        mu0[is_single] = 0.5
+        mu0[~is_single] = (mu0[~is_single] - raw_bounds[~is_single, 0]) / domain_sizes[~is_single]
+
+        cov0 = self._cov0 / (domain_sizes * domain_sizes[:, np.newaxis])
+        sigma0 = math.pow(np.linalg.det(cov0), 1.0 / 2.0 / dim)
+        # Avoid ZeroDivisionError in cmaes.
+        sigma0 = max(self._sigma0, 1e-10)
+
+        # Make the determinant of cov0 1 so that it agrees with the CMA-ES convention.
+        cov0 /= sigma0**2
+
+        return mu0, sigma0, cov0
+
     def _init_optimizer(
         self,
         trans: _SearchSpaceTransform,
@@ -126,11 +157,7 @@ class UserPriorCmaEsSampler(CmaEsSampler):
         randomize_start_point: bool = False,
     ) -> CmaClass:
         n_dimension = len(trans.bounds)
-        mean = self._mu0.copy()
-        cov = self._cov0.copy()
-
-        # Avoid ZeroDivisionError in cmaes.
-        sigma0 = max(self._sigma0, 1e-10)
+        mu0, sigma0, cov0 = self._calculate_initial_params(trans._search_space)
 
         if self._with_margin:
             steps = np.empty(len(trans._search_space), dtype=float)
@@ -145,20 +172,20 @@ class UserPriorCmaEsSampler(CmaEsSampler):
                     steps[i] = dist.step / (dist.high - dist.low)
 
             return cmaes.CMAwM(
-                mean=mean,
+                mean=mu0,
                 sigma=sigma0,
                 bounds=trans.bounds,
                 steps=steps,
-                cov=cov,
+                cov=cov0,
                 seed=self._cma_rng.rng.randint(1, 2**31 - 2),
                 n_max_resampling=10 * n_dimension,
                 population_size=population_size,
             )
 
         return cmaes.CMA(
-            mean=mean,
+            mean=mu0,
             sigma=sigma0,
-            cov=cov,
+            cov=cov0,
             bounds=trans.bounds,
             seed=self._cma_rng.rng.randint(1, 2**31 - 2),
             n_max_resampling=10 * n_dimension,
