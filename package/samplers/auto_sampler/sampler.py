2a7d4bb047975a0365ee4d68c267eed9abd8d2e9
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2024-10-18 05:51:57 +0200

    Update auto sampler

--- package/samplers/auto_sampler/sampler.py
+++ package/samplers/auto_sampler/sampler.py
@@ -1,8 +1,8 @@
 from __future__ import annotations
 
+from collections.abc import Callable
 from collections.abc import Sequence
 from typing import Any
-from typing import Callable
 from typing import TYPE_CHECKING
 
 from optuna.distributions import CategoricalDistribution
