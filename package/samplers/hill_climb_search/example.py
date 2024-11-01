db86207595634a0764c72218e52ef34454edb98d
Chinmaya Sahu <chinmayasahu101@gmail.com>
2024-11-01 20:05:55 +0530

    Changed examples

--- package/samplers/hill_climb_search/example.py
+++ package/samplers/hill_climb_search/example.py
@@ -1,15 +1,17 @@
 import optuna
 import optunahub
 
-if __name__ == "__main__":
-    mod = optunahub.load_module(package="samplers/hill_climb_search")
-    
+if __name__ == "__main__":    
     def objective(trial):
         x = trial.suggest_discrete_uniform("x", -10, 10)
         y = trial.suggest_discrete_uniform("y", -10, 10)
         return -(x**2 + y**2)
 
-    sampler = mod.HillClimbSearch()
+    module = optunahub.load_module(
+        package="samplers/hill-climb-search",
+        repo_owner="csking101",
+        ref="hill-climb-algorithm")
+    sampler = module.HillClimbSearch()
     study = optuna.create_study(sampler=sampler)
     study.optimize(objective, n_trials=20)
 
