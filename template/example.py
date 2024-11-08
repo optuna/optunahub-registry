6ff8e63b47ff21aa4240e50af83837ba9df084ab
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2024-11-08 08:47:18 +0100

    Fix

--- template/example.py
+++ template/example.py
@@ -12,7 +12,7 @@ import optunahub
 
 def objective(trial: optuna.Trial) -> float:
     x = trial.suggest_float("x", -5, 5)
-    y = trial.suggest_float("x", -5, 5)
+    y = trial.suggest_float("y", -5, 5)
     return x**2 + y**2
 
 
