13b197113e4d394bf884fdc002da4cd22c629112
Bryon Tjanaka <bryon.tjanaka@gmail.com>
2024-11-21 15:09:31 -0800

    Update example

--- package/samplers/cmamae/example.py
+++ package/samplers/cmamae/example.py
@@ -3,35 +3,32 @@ from optuna.study import StudyDirection
 from sampler import CmaMaeSampler
 
 
-# TODO: Replace above import with this.
-#  module = optunahub.load_module("samplers/pyribs")
-#  PyribsSampler = module.PyribsSampler
-
-
 def objective(trial: optuna.trial.Trial) -> tuple[float, float, float]:
     """Returns an objective followed by two measures."""
     x = trial.suggest_float("x", -10, 10)
     y = trial.suggest_float("y", -10, 10)
-    return -(x**2 + y**2) + 2, x, y
+    return x**2 + y**2, x, y
 
 
 if __name__ == "__main__":
     sampler = CmaMaeSampler(
         param_names=["x", "y"],
         archive_dims=[20, 20],
-        archive_ranges=[(-10, 10), (-10, 10)],
+        archive_ranges=[(-1, 1), (-1, 1)],
         archive_learning_rate=0.1,
         archive_threshold_min=-10,
         n_emitters=1,
-        emitter_x0={"x": 5, "y": 5},
+        emitter_x0={
+            "x": 0,
+            "y": 0,
+        },
         emitter_sigma0=0.1,
-        emitter_batch_size=5,
+        emitter_batch_size=20,
     )
     study = optuna.create_study(
         sampler=sampler,
         directions=[
-            # pyribs maximizes objectives.
-            StudyDirection.MAXIMIZE,
+            StudyDirection.MINIMIZE,
             # The remaining values are measures, which do not have an
             # optimization direction.
             # TODO: Currently, using StudyDirection.NOT_SET is not allowed as
@@ -40,8 +37,4 @@ if __name__ == "__main__":
             StudyDirection.MINIMIZE,
         ],
     )
-    study.optimize(objective, n_trials=100)
-
-    # TODO: Visualization.
-    #  fig = optuna.visualization.plot_optimization_history(study)
-    #  fig.write_image("cmamae_optimization_history.png")
+    study.optimize(objective, n_trials=10000)
