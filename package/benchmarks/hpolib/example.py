2a7fda4c81b786cd2a4a1bdc73cbe1d144eb59c1
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2025-01-07 05:28:54 +0100

    Update example

--- package/benchmarks/hpolib/example.py
+++ package/benchmarks/hpolib/example.py
@@ -1,30 +1,11 @@
-"""
-This example is only for sampler.
-You can verify your sampler code using this file as well.
-Please feel free to remove this file if necessary.
-"""
-
 from __future__ import annotations
 
 import optuna
 import optunahub
 
 
-# TODO: Change package_name to test your package.
-package_name = "benchmarks/hpolib"
-test_local = True
-
-if test_local:
-    hpolib = optunahub.load_local_module(
-        package=package_name,
-        registry_root="./package",
-    )
-else:
-    hpolib = optunahub.load_module(
-        package=package_name, repo_owner="nabenabe0928", ref="add-hpolib"
-    )
-
-problem = hpolib.Problem(dataset_id=0)
+hpobench = optunahub.load_module("benchmarks/hpolib")
+problem = hpobench.Problem(dataset_id=0)
 study = optuna.create_study()
 study.optimize(problem, n_trials=30)
-print(study.best_trials)
+print(study.best_trial)
