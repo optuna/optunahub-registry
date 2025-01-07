f589dae5c253bbd6ef1f03aa54586a7df8503780
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2025-01-07 05:28:01 +0100

    Update example

--- package/benchmarks/hpobench_nn/example.py
+++ package/benchmarks/hpobench_nn/example.py
@@ -1,30 +1,11 @@
-"""
-This example is only for sampler.
-You can verify your sampler code using this file as well.
-Please feel free to remove this file if necessary.
-"""
-
 from __future__ import annotations
 
 import optuna
 import optunahub
 
 
-# TODO: Change package_name to test your package.
-package_name = "benchmarks/hpobench_nn"
-test_local = True
-
-if test_local:
-    hpobench = optunahub.load_local_module(
-        package=package_name,
-        registry_root="./package",
-    )
-else:
-    hpobench = optunahub.load_module(
-        package=package_name, repo_owner="nabenabe0928", ref="add-hpobench"
-    )
-
+hpobench = optunahub.load_module("benchmarks/hpobench_nn")
 problem = hpobench.Problem(dataset_id=0)
 study = optuna.create_study()
 study.optimize(problem, n_trials=30)
-print(study.best_trials)
+print(study.best_trial)
