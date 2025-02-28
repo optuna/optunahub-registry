83675b297d44d875d45ad5b4f62d03f61ceab2e5
JInglue <jingluexu@gmail.com>
2025-02-28 16:29:22 +0900

    rolled back

--- package/samplers/llambo/__init__.py
+++ package/samplers/llambo/__init__.py
@@ -1,14 +1,4 @@
-import os
-import sys
+from .sampler_base import LLAMBOSampler
 
 
-# Add the current directory to sys.path
-current_dir = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, current_dir)
-
-# Now we can import with standard paths
-from sampler_base import LLAMBOSampler
-
-
-# Export the main class
 __all__ = ["LLAMBOSampler"]
