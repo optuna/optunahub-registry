b9be9f187435e290767022d7049104f7ecb2a7cc
malun22 <malun22@web.de>
2025-09-15 13:09:25 +0200

    Use PSO

--- package/samplers/particle_swarm_optimization/__init__.py
+++ package/samplers/particle_swarm_optimization/__init__.py
@@ -1,4 +1,4 @@
-from .YOUR_TEMPLATE import YOUR_FEATURE
+from .sampler import PSOSampler
 
 
-__all__ = ["YOUR_FEATURE"]
+__all__ = ["PSOSampler"]
--- package/samplers/particle_swarm_optimization/example.py
+++ package/samplers/particle_swarm_optimization/example.py
@@ -16,23 +16,43 @@ def objective(trial: optuna.Trial) -> float:
     return x**2 + y**2
 
 
-# TODO: Change package_name to test your package.
-package_name = "samplers/your_sampler"
+package_name = "samplers/particle_swarm_optimization"
 test_local = True
+
+n_trials = 100
+n_generations = 5
 
 if test_local:
     # This is an example of how to load a sampler from your local optunahub-registry.
     sampler = optunahub.load_local_module(
         package=package_name,
         registry_root="./package",  # Path to the root of the optunahub-registry.
-    ).YourSampler()
+    ).PSOSampler(
+        {
+            "x": optuna.distributions.FloatDistribution(-10, 10),
+            "y": optuna.distributions.FloatDistribution(-10, 10, step=0.1),
+        },
+        n_particles=int(n_trials / n_generations),
+        inertia=0.5,
+        cognitive=1.5,
+        social=1.5,
+    )
 else:
     # This is an example of how to load a sampler from your fork of the optunahub-registry.
     # Please remove repo_owner and ref arguments before submitting a pull request.
     sampler = optunahub.load_module(
         package=package_name, repo_owner="Your GitHub Account ID", ref="Your Git Branch Name"
-    ).YourSampler()
+    ).PSOSampler(
+        {
+            "x": optuna.distributions.FloatDistribution(-10, 10),
+            "y": optuna.distributions.FloatDistribution(-10, 10, step=0.1),
+        },
+        n_particles=int(n_trials / n_generations),
+        inertia=0.5,
+        cognitive=1.5,
+        social=1.5,
+    )
 
 study = optuna.create_study(sampler=sampler)
-study.optimize(objective, n_trials=30)
+study.optimize(objective, n_trials=n_trials)
 print(study.best_trials)
