4c8ccfea4267e03420d29f323f3bad43a30954af
Shinichi Hemmi <50256998+Alnusjaponica@users.noreply.github.com>
2025-09-29 11:32:09 +0900

    Add an example

--- package/benchmarks/multidimensional_knapsack/example.py
+++ package/benchmarks/multidimensional_knapsack/example.py
@@ -4,9 +4,12 @@ import optuna
 import optunahub
 
 
-mkp_module = optunahub.load_module("benchmarks/multidimensional_knapsack")
+mkp_module = optunahub.load_module(
+    "benchmarks/multidimensional_knapsack",
+)
 
-problem = mkp_module.Problem(n_items=15, n_dimensions=3, seed=42)
-study = optuna.create_study(directions=problem.directions)
+problem = mkp_module.Problem(n_items=100, n_dimensions=10, seed=42)
+sampler = optuna.samplers.NSGAIISampler(constraints_func=problem.evaluate_constraints)
+study = optuna.create_study(directions=problem.directions, sampler=sampler)
 study.optimize(problem, n_trials=100)
 print(study.best_trial)
