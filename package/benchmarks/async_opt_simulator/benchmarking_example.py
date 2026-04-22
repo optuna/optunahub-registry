91bc9b54e155cf6e3904178717bdf2a626dc1b5c
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2026-04-22 14:21:47 +0900

    Update example

--- package/benchmarks/async_opt_simulator/benchmarking_example.py
+++ package/benchmarks/async_opt_simulator/benchmarking_example.py
@@ -3,27 +3,26 @@ from __future__ import annotations
 import itertools
 
 import matplotlib.pyplot as plt
-import numpy as np
 import optuna
 import optunahub
 
 
-plt.rcParams["font.family"] = "Times New Roman"
-plt.rcParams["font.size"] = 14
-plt.rcParams["mathtext.fontset"] = "stix"
+plot_target_over_time = optunahub.load_module(
+    "visualization/plot_target_over_time"
+).plot_target_over_time
+Problem = optunahub.load_module("benchmarks/hpolib").Problem
+AsyncOptBenchmarkSimulator = optunahub.load_module(
+    "benchmarks/async_opt_simulator"
+).AsyncOptBenchmarkSimulator
 
 
-def simulate(n_workers: int, constant_liar: bool, seed: int) -> tuple[np.ndarray, np.ndarray]:
-    sim = optunahub.load_module("benchmarks/async_opt_simulator").AsyncOptBenchmarkSimulator(
-        n_workers=n_workers
-    )
-    Problem = optunahub.load_module("benchmarks/hpolib").Problem
+def simulate(n_workers: int, constant_liar: bool, seed: int) -> optuna.Study:
+    sim = AsyncOptBenchmarkSimulator(n_workers=n_workers)
     problem = Problem(dataset_id=0, metric_names=["val_loss"], seed=0)
     runtime_func = Problem(dataset_id=0, metric_names=["train_time"], seed=0)
-    n_init = 10
     sampler = optuna.samplers.TPESampler(
         multivariate=True,
-        n_startup_trials=n_init,
+        n_startup_trials=(n_init := 10),
         constant_liar=constant_liar,
         seed=seed,
     )
@@ -31,54 +30,32 @@ def simulate(n_workers: int, constant_liar: bool, seed: int) -> tuple[np.ndarray
     sim.optimize(
         study=study, problem=problem, runtime_func=lambda t: runtime_func(t)[0], n_trials=100
     )
-    results = sim.get_results_from_study(study)
-    return np.array(results["cumtime"])[n_init:], np.array(results["values"])[n_init:]
-
-
-def get_values_on_fixed_time_steps(
-    cumtimes: np.ndarray, values: np.ndarray
-) -> tuple[np.ndarray, np.ndarray]:
-    t_min = np.min(cumtimes)
-    t_max = np.max(cumtimes)
-    ts = np.exp(np.linspace(np.log(t_min), np.log(t_max)))
-    v_on_grid = []
-    for ct, v in zip(cumtimes, values):
-        i_upper = np.minimum(np.searchsorted(ct, ts, side="left"), v.size - 1)
-        v_on_grid.append(v[i_upper])
-    return ts, np.array(v_on_grid)
+    new_study = optuna.create_study()
+    new_study.add_trials(study.trials[n_init:])
+    return new_study
 
 
 def plot(ax: plt.Axes, n_workers: int, constant_liar: bool) -> None:
     colors = {1: "black", 2: "blue", 4: "darkred"}
-    markers = {True: "*", False: "s"}
-    linestyles = {True: "dotted", False: "dashed"}
-    label = f"$n=${n_workers}, {constant_liar=}"
-    color = colors[n_workers]
-    marker = markers[constant_liar]
-    ls = linestyles[constant_liar]
-    cumtime_list = []
-    value_list = []
-    for seed in range(2):
-        cumtime, values = simulate(n_workers, constant_liar, seed=seed)
-        cumtime_list.append(cumtime)
-        value_list.append(np.minimum.accumulate(values[:, 0]))
-    ts, vs = get_values_on_fixed_time_steps(np.array(cumtime_list), np.array(value_list))
-    m = np.mean(vs, axis=0)
-    s = np.std(vs, axis=0) / np.sqrt(len(vs))
-    ax.plot(ts, m, color=color, marker=marker, markevery=10, ls=ls, label=label)
-    ax.fill_between(ts, m - s, m + s, color=color, alpha=0.2)
+    study_list = []
+    for seed in range(10):
+        study_list.append(simulate(n_workers, constant_liar, seed=seed))
+    plot_target_over_time(
+        study_list,
+        color=colors[n_workers],
+        ax=ax,
+        cumtime_func=lambda t: t.user_attrs["cumtime"],
+        label=f"$n=${n_workers}, {constant_liar=}",
+        marker="*" if constant_liar else "s",
+        ls="dotted" if constant_liar else "dashed",
+        markevery=10,
+    )
 
 
 if __name__ == "__main__":
     _, ax = plt.subplots()
-    ax.set_xscale("log")
-    ax.set_yscale("log")
-    ax.grid(which="minor", color="gray", linestyle=":")
-    ax.grid(which="major", color="black")
-    ax.set_xlabel("Wallclock Time [s]")
-    ax.set_ylabel("Validation Loss")
     for n_workers, constant_liar in itertools.product(*([1, 2, 4], [True, False])):
         plot(ax, n_workers=n_workers, constant_liar=constant_liar)
-
     ax.legend()
+    ax.set_yscale("log")
     plt.savefig("async-bench-example.png", bbox_inches="tight")
