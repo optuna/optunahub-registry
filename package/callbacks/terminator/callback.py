87edd6af4eefca6951028e1520a614a2e44583d8
gen740 <54583542+gen740@users.noreply.github.com>
2026-06-10 16:27:29 +0900

    Port terminator from Optuna
    Source:
    https://github.com/optuna/optuna/tree/6ace9ab96a2ac9a8a9e170d744e3737edb6b37ad/optuna/terminator
    
    Co-authored-by: Contramundum <contramundum53@outlook.com>
    Co-authored-by: Eri Sawada <esawada2003@gmail.com>
    Co-authored-by: Hideaki Imamura <38826298+HideakiImamura@users.noreply.github.com>
    Co-authored-by: HideakiImamura <mamu@preferred.jp>
    Co-authored-by: Hiroki Takizawa <contact@hiroki-takizawa.name>
    Co-authored-by: Hiroyoshi Komatsu <torotoki@users.noreply.github.com>
    Co-authored-by: Hiroyuki Vincent Yamazaki <hiroyuki.vincent.yamazaki@gmail.com>
    Co-authored-by: Kaito Suzuki <34023087+cross32768@users.noreply.github.com>
    Co-authored-by: Kento Nozawa <k_nzw@klis.tsukuba.ac.jp>
    Co-authored-by: Kento Nozawa <nzw0301@preferred.jp>
    Co-authored-by: Naoto Mizuno <naotomizuno@preferred.jp>
    Co-authored-by: Shinichi Hemmi <50256998+Alnusjaponica@users.noreply.github.com>
    Co-authored-by: Shotaro Sano <s.g.votte@gmail.com>
    Co-authored-by: Shuhei Watanabe <47781922+nabenabe0928@users.noreply.github.com>
    Co-authored-by: Sip4818 <sip4818@gmail.com>
    Co-authored-by: Toshihiko Yanase <toshihiko.yanase@gmail.com>
    Co-authored-by: Virendra Patil <virendrapatil.24vp@gmail.com>
    Co-authored-by: Yoshihiko Ozaki <30489874+y0z@users.noreply.github.com>
    Co-authored-by: c-bata <contact@c-bata.link>
    Co-authored-by: cross32768 <suzukikaito14.ne.jp@gmail.com>
    Co-authored-by: gen740 <54583542+gen740@users.noreply.github.com>
    Co-authored-by: hvy <hiroyuki.vincent.yamazaki@gmail.com>
    Co-authored-by: kAIto47802 <115693559+kAIto47802@users.noreply.github.com>
    Co-authored-by: nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
    Co-authored-by: outbreak_kp <112833146+jot-s-bindra@users.noreply.github.com>
    Co-authored-by: smygw <smygw72@gmail.com>
    Co-authored-by: t7r0n <encom@Lenovo-Y740.localdomain>

---
+++ package/callbacks/terminator/callback.py
@@ -0,0 +1,87 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from optuna._deprecated import _DEPRECATION_WARNING_TEMPLATE
+from optuna._warnings import optuna_warn
+from optuna.logging import get_logger
+
+from .terminator import Terminator
+
+
+if TYPE_CHECKING:
+    from optuna.study.study import Study
+    from optuna.trial import FrozenTrial
+
+    from .terminator import BaseTerminator
+
+
+_logger = get_logger(__name__)
+
+_DEPRECATION_WARNING_MESSAGE = _DEPRECATION_WARNING_TEMPLATE.format(
+    name="`optuna.terminator` module",
+    d_ver="4.9.0",
+    r_ver="6.0.0",
+)
+
+
+class TerminatorCallback:
+    """A callback that terminates the optimization using Terminator.
+
+    This class implements a callback which wraps :class:`~optuna.terminator.Terminator`
+    so that it can be used with the :func:`~optuna.study.Study.optimize` method.
+
+    Args:
+        terminator:
+            A terminator object which determines whether to terminate the optimization by
+            assessing the room for optimization and statistical error. Defaults to a
+            :class:`~optuna.terminator.Terminator` object with default
+            ``improvement_evaluator`` and ``error_evaluator``.
+
+    Example:
+
+        .. testcode::
+
+            from sklearn.datasets import load_wine
+            from sklearn.ensemble import RandomForestClassifier
+            from sklearn.model_selection import cross_val_score
+            from sklearn.model_selection import KFold
+
+            import optuna
+            from optuna.terminator import TerminatorCallback
+            from optuna.terminator import report_cross_validation_scores
+
+
+            def objective(trial):
+                X, y = load_wine(return_X_y=True)
+
+                clf = RandomForestClassifier(
+                    max_depth=trial.suggest_int("max_depth", 2, 32),
+                    min_samples_split=trial.suggest_float("min_samples_split", 0, 1),
+                    criterion=trial.suggest_categorical("criterion", ("gini", "entropy")),
+                )
+
+                scores = cross_val_score(clf, X, y, cv=KFold(n_splits=5, shuffle=True))
+                report_cross_validation_scores(trial, scores)
+                return scores.mean()
+
+
+            study = optuna.create_study(direction="maximize")
+            terminator = TerminatorCallback()
+            study.optimize(objective, n_trials=50, callbacks=[terminator])
+
+    .. seealso::
+        Please refer to :class:`~optuna.terminator.Terminator` for the details of
+        the terminator mechanism.
+    """
+
+    def __init__(self, terminator: BaseTerminator | None = None) -> None:
+        optuna_warn(_DEPRECATION_WARNING_MESSAGE, FutureWarning)
+        self._terminator = terminator or Terminator()
+
+    def __call__(self, study: Study, trial: FrozenTrial) -> None:
+        should_terminate = self._terminator.should_terminate(study=study)
+
+        if should_terminate:
+            _logger.info("The study has been stopped by the terminator.")
+            study.stop()
