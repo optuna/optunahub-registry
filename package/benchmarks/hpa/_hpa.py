60854b950e916e31e9697ef62bb80a0c7a8fe13e
Naoto Mizuno <naotomizuno@preferred.jp>
2026-08-26 19:37:50 +0900

    Revert changes for hpa

--- package/benchmarks/hpa/_hpa.py
+++ package/benchmarks/hpa/_hpa.py
@@ -7,7 +7,6 @@ from typing import TYPE_CHECKING
 import numpy as np
 import optuna
 import optunahub
-from packaging import version
 
 from .hpa_original import problem
 
@@ -136,7 +135,7 @@ class Problem(optunahub.benchmarks.BaseProblem):
         return getattr(self._problem, name)
 
 
-class ConstrainedProblem(optunahub.benchmarks.BaseProblem):
+class ConstrainedProblem(optunahub.benchmarks.ConstrainedMixin, optunahub.benchmarks.BaseProblem):
     def __init__(self, problem_name: str, n_div: int = 4, level: int = 0) -> None:
         """Initialize the problem.
         Args:
@@ -147,13 +146,6 @@ class ConstrainedProblem(optunahub.benchmarks.BaseProblem):
         Please refer to the hpa repository for the details.
         https://github.com/Nobuo-Namura/hpa
         """
-        if version.parse(optunahub.__version__).release < (0, 5, 0):
-            raise RuntimeError(
-                "This problem requires OptunaHub v0.5.0 or newer to handle its constraints, "
-                f"but v{optunahub.__version__} is installed. "
-                "Please upgrade it by `pip install -U optunahub`."
-            )
-
         if problem_name not in constrained_problems:
             raise ValueError(
                 f"problem_name must be in {list(constrained_problems.keys())}, "
@@ -190,9 +182,8 @@ class ConstrainedProblem(optunahub.benchmarks.BaseProblem):
     def __getattr__(self, name: str) -> Any:
         return getattr(self._problem, name)
 
-    def evaluate_constraints(self, params: dict[str, float]) -> dict[str, float]:
-        constraints = self._problem([params[name] for name in self._search_space])[1].tolist()
-        return {str(i): c for i, c in enumerate(constraints)}
+    def evaluate_constraints(self, params: dict[str, float]) -> list[float]:
+        return self._problem([params[name] for name in self._search_space])[1].tolist()
 
 
 if TYPE_CHECKING:
